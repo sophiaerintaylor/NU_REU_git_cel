ddbd7bfa0d34ed1a28ced1262bc31244aef8c3f1
Camille Liotine <camille@dhcp-10-105-220-63.wireless.northwestern.private>
2019-07-08 13:34:50 -0500

    added plot script

---
+++ python_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.linspace(0, 2*np.pi, 100)
+
+fig, ax = plt.subplots(figsize=(8,5))
+ax.plot(x, np.sin(x))
+ax.set_xlim(0, 2*np.pi)
+plt.show()
